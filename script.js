d4d7b60c48e4be48da15da2ce05c393dcc0553c7
Dhvanit <monparadhvanit@gmail.com>
2023-12-09 18:54:30 +0530

    Major Ui changes except navigation bar

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-  const tabSelect = document.querySelectorAll('.navTab');
-  const tabSwitch = document.querySelectorAll('.tabSelector');
+document.addEventListener("DOMContentLoaded", function () {
+  const tabSelect = document.querySelectorAll(".navTab");
+  const tabSwitch = document.querySelectorAll(".tabSelector");
 
   tabSelect.forEach(function (item) {
-    item.addEventListener('click', function () {
-
+    item.addEventListener("click", function () {
       // Remove 'active' class from all tabs
-      tabSwitch.forEach(function(disable) {
+      tabSwitch.forEach(function (disable) {
+        disable.classList.remove("active");
+        disable.classList.add("hover:bg-slate-800");
 
-        disable.classList.remove('active')
-        disable.classList.add('hover:bg-slate-800')
+        const colorSwitchWhite = disable.querySelectorAll(".iconCol");
+        colorSwitchWhite.forEach(function (toggle) {
+          toggle.setAttribute("fill", "#ffffff");
+        });
 
-        const colorSwitchWhite = disable.querySelectorAll('.iconCol')
-        colorSwitchWhite.forEach(function(toggle) {
-          toggle.setAttribute('fill', '#ffffff')
-          
-        })
-
-        const conHide = document.querySelectorAll('.conSelect')
-        conHide.forEach(Hidden => Hidden.setAttribute('style', 'display: none;'))
-
+        const conHide = document.querySelectorAll(".conSelect");
+        conHide.forEach((Hidden) =>
+          Hidden.setAttribute("style", "display: none;")
+        );
       });
 
       // Identify the clicked tab and activate it
@@ -30,20 +27,16 @@ document.addEventListener('DOMContentLoaded', function () {
       // innerTabActivate(clickedTab)
 
       function tabActivate(tabId) {
-        tabId.classList.add('active');
-        tabId.classList.remove('hover:bg-slate-800')
+        tabId.classList.add("active");
+        tabId.classList.remove("hover:bg-slate-800");
 
-        const activeTab = document.querySelector('.active')
+        const activeTab = document.querySelector(".active");
 
-        const colorSwitchBlack = activeTab.querySelectorAll('.iconCol')
-        colorSwitchBlack.forEach(function(toggle) {
-
-          toggle.setAttribute('fill', '#000000')
-
-        })
-
+        const colorSwitchBlack = activeTab.querySelectorAll(".iconCol");
+        colorSwitchBlack.forEach(function (toggle) {
+          toggle.setAttribute("fill", "#000000");
+        });
       }
     });
   });
-})
-
+});
